e660b7a35bdeebfb32807a359c090f65e5f75c15
kosma2 <115192704+kosma2@users.noreply.github.com>
2024-06-02 13:40:22 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -7,26 +7,37 @@ namespace deliver
     {
         public static void Main()
         {
-            System.Console.WriteLine("1.admin 2.user");
+            System.Console.WriteLine("1.admin 2.user 3.nav");
             string a = Console.ReadLine();
-            switch(a)
+            switch (a)
             {
                 case "1":
-                IAdminLogin();
-                break;
+                    IAdminLogin();
+                    break;
                 case "2":
-                IUserLogin();
-                break;
+                    IUserLogin();
+                    break;
+                case "3":
+                    Nav();
+                    break;
             }
-            /*var config = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json")
-                    .Build();
+
+        }
+        public static void Nav()
+        {
+            var config = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
             UserSession uSession = null;
             GeoConnect geoConnection = new();
-                        geoConnection.SqlStr = config.GetConnectionString("DefaultConnection");
-                        double distance = geoConnection.GetDistance(1004,3);
-                        System.Console.WriteLine($"distance between them is {distance}m.");*/
+            geoConnection.SqlStr = config.GetConnectionString("DefaultConnection");
+            //geoConnection.GetNodeDistance("n1", "n2");
+            List<Node> nodeList = geoConnection.DBGetGraphData();
+            
+            var dijkstra = new Dijkstra();
+            Node startingNode = nodeList.FirstOrDefault(node => node.Id == "n1");
+            dijkstra.Execute(startingNode, nodeList);
         }
     }
 }
